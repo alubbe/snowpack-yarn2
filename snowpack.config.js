129f88423ca753f9cecfd27385792aa160091ea8
Andreas Lubbe <git@lubbe.org>
2020-08-03 09:35:55 +0200

    Initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+const commonjs = require('@rollup/plugin-commonjs')
+const nodePolyfills = require('rollup-plugin-node-polyfills');
+
+module.exports = {
+  extends: "@snowpack/app-scripts-react",
+  scripts: {},
+  plugins: [],
+  devOptions: {
+    open: 'none'
+  },
+  buildOptions: {
+    baseUrl: `${process.env.PUBLIC_URL}/`,
+  },
+  installOptions: {
+    rollup: {
+      plugins: [nodePolyfills(), commonjs()] // nodePolyfills must be before commonjs or we get an infinite recursion
+    }
+  }
+}
